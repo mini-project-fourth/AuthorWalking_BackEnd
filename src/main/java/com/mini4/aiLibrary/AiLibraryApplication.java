b6d214bff54327a21d7b9b51a5ec33943ca261dc
heeji <heeji0502@gmail.com>
2025-05-29 15:51:34 +0900

    initial commit

---
+++ src/main/java/com/mini4/aiLibrary/AiLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.mini4.aiLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiLibraryApplication.class, args);
+	}
+
+}
