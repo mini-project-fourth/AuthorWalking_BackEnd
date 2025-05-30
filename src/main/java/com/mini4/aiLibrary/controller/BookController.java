81a74c4ec44e0b82f6853ed07158e9cb60eb0bc1
heeji <heeji0502@gmail.com>
2025-05-30 11:16:01 +0900

    feat: create Book

---
+++ src/main/java/com/mini4/aiLibrary/controller/BookController.java
@@ -0,0 +1,26 @@
+package com.mini4.aiLibrary.controller;
+
+import com.mini4.aiLibrary.domain.Book;
+import com.mini4.aiLibrary.dto.BookDto;
+import com.mini4.aiLibrary.service.BookService;
+import com.mini4.aiLibrary.service.BookServiceImpl;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping("/books")
+    public Book createBook(@RequestBody BookDto.BookPost bookDto) {
+        return bookService.insertBook(bookDto);
+    }
+
+}
---
+++ src/main/java/com/mini4/aiLibrary/dto/BookDto.java
@@ -0,0 +1,25 @@
+package com.mini4.aiLibrary.dto;
+
+import com.mini4.aiLibrary.domain.Book;
+import lombok.*;
+
+public class BookDto {
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class BookPost {
+
+        private String title;
+        private String contents;
+
+        @Builder
+        public Book toEntity() {
+            return Book.builder()
+                    .title(title)
+                    .contents(contents)
+                    .build();
+        }
+    }
+}
